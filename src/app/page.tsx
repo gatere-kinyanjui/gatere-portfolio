45843ad98520195a0a55b61cb4d9f13b79cad05f
GatereKinyanjui <gaterekinyanjui@gmail.com>
2024-02-13 22:22:24 +0300

    completes home barebones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,60 @@
 import Image from "next/image";
+import homeIcon from "../../public/home.svg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Personal Portfolio Coming
-          </p>
-        </a>
+    <main>
+      <div>
+        <header>
+          <Image alt="home icon" src={homeIcon} width={32} height={32} />
+
+          <nav>
+            <ul>
+              <Link href={""}>
+                <li>Skills</li>
+              </Link>
+
+              <Link href={""}>
+                <li>Work Experience</li>
+              </Link>
+              <Link href={""}>
+                <li>Education</li>
+              </Link>
+            </ul>
+          </nav>
+        </header>
+
+        <div>
+          <h3>Hello! Welcome to my porfolio. My name is Gatere</h3>
+          <p>How I have Served Others</p>
+          <Link href={""}>
+            <li>Projects</li>
+          </Link>
+        </div>
+
+        <footer>
+          <nav>
+            <ul>
+              <Link href={"https://github.com/gatere-kinyanjui"} scroll={false}>
+                <li>Github</li>
+              </Link>
+              <Link
+                href={
+                  "https://www.linkedin.com/in/elvis-gideon-gatere-724514138/"
+                }
+                scroll={false}
+              >
+                <li>LinkedIn</li>
+              </Link>
+              <li>
+                <Link href={""} scroll={false}>
+                  <button>Hire me</button>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </footer>
       </div>
     </main>
   );
