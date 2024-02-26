c65e772d4606f10687d7a5763ff236fbe101c577
GatereKinyanjui <gaterekinyanjui@gmail.com>
2024-02-26 15:09:27 +0300

    repairs maintenance deployment deployment error

--- src/app/(pages)/education/page.tsx
+++ src/app/(pages)/education/page.tsx
@@ -1,9 +1,10 @@
+import EducationModal from "@/app/components/display-components/EducationModal";
 import React from "react";
 
 const Education = () => {
   return (
     <div>
-      < />
+      <EducationModal />
     </div>
   );
 };
